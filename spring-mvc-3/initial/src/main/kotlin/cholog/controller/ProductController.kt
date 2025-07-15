a7a0ed957b4faed1f1549d5e9512aaa8bb0cdc6c
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-15 14:14:17 +0200

    feat: Complete MVC-3

--- spring-mvc-3/initial/src/main/kotlin/cholog/controller/ProductController.kt
+++ spring-mvc-3/initial/src/main/kotlin/cholog/controller/ProductController.kt
@@ -2,10 +2,7 @@ package cholog.controller
 
 import cholog.exception.NotFoundException
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class ProductController {
@@ -27,5 +24,11 @@ class ProductController {
         return ResponseEntity.ok().build()
     }
 
-    // TODO: Handle IllegalArgumentException using @ExceptionHandler
+    @ExceptionHandler
+    fun handleIllegalArgumentException(
+        e: IllegalArgumentException
+    ): ResponseEntity<Void> {
+        println("IllegalArgumentException" + e.message)
+        return ResponseEntity.badRequest().build()
+    }
 }
--- spring-mvc-3/initial/src/main/kotlin/cholog/exception/NotFoundException.kt
+++ spring-mvc-3/initial/src/main/kotlin/cholog/exception/NotFoundException.kt
@@ -1,3 +1,4 @@
 package cholog.exception
 
+
 class NotFoundException(message: String) : RuntimeException(message)
