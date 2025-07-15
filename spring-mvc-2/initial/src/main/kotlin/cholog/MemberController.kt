133556f129ac4c56e8f7cce82ca57cc4f6cee0f4
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-07-15 13:52:01 +0200

    feat: Complete MVC-2

--- spring-mvc-2/initial/src/main/kotlin/cholog/MemberController.kt
+++ spring-mvc-2/initial/src/main/kotlin/cholog/MemberController.kt
@@ -2,6 +2,13 @@ package cholog
 
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import java.net.URI
 import java.util.concurrent.atomic.AtomicLong
 
 @Controller
@@ -9,38 +16,46 @@ class MemberController {
     private val members: MutableList<Member> = ArrayList()
     private val index = AtomicLong(1)
 
-    fun create(): ResponseEntity<Void> {
-        // TODO: create Member
-//        val newMember: Member = Member.toEntity(member, index.getAndIncrement())
-//        members.add(newMember)
-        return ResponseEntity.badRequest().build()
+    @PostMapping("/members")
+    fun create(
+        @RequestBody member: Member
+    ): ResponseEntity<Member> {
+        val newMember: Member = Member.toEntity(member, index.getAndIncrement())
+        members.add(newMember)
+        val location = URI.create("/users/${newMember.id}")
+        return ResponseEntity.created(location).body(newMember)
     }
 
+    @GetMapping("/members")
     fun read(): ResponseEntity<List<Member>> {
-        // TODO: Return all stored members.
-        return ResponseEntity.badRequest().build()
+        return ResponseEntity.ok(members)
     }
 
-    fun update(): ResponseEntity<Void> {
-        // TODO: Update member using the request body and the ID from the URL.
-//        val member = members.stream()
-//            .filter { it: Member -> it.id == id }
-//            .findFirst()
-//            .orElseThrow { RuntimeException() }
-//
-//        member.update(newMember)
-        return ResponseEntity.badRequest().build()
+    @PutMapping("members/{id}")
+    fun update(
+        @PathVariable id: Long,
+        @RequestBody newMember: Member,
+    ): ResponseEntity<Member> {
+        val member = members.stream()
+            .filter { it: Member -> it.id == id }
+            .findFirst()
+            .orElseThrow { RuntimeException() }
+
+        member.update(newMember)
+        return ResponseEntity.ok().body(member)
     }
 
-    fun delete(): ResponseEntity<Void> {
-        // TODO: Delete the member using the ID from the URL.
-//        val member = members.stream()
-//            .filter { it: Member -> it.id == id }
-//            .findFirst()
-//            .orElseThrow { RuntimeException() }
-//
-//        members.remove(member)
+    @DeleteMapping("members/{id}")
+    fun delete(
+        @PathVariable id: Long,
+    ): ResponseEntity<Void> {
+        val member = members.stream()
+            .filter { it: Member -> it.id == id }
+            .findFirst()
+            .orElseThrow { RuntimeException() }
 
-        return ResponseEntity.badRequest().build()
+        members.remove(member)
+
+        return ResponseEntity.noContent().build()
     }
 }
